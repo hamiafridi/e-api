89e4b37787c350cd2aa20fdb87a3874687134dec
Hamayun Afridi <hamayun.afridi99@gmail.com>
2018-04-23 23:18:22 -0700

    Relations Created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,14 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Model\review;
+
 
 class Product extends Model
 {
-    //
+    
+	public function reviews() {
+		return $this->hasMany(Review::class);
+	}
+
 }
