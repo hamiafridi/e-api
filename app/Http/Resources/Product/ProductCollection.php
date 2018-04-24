1e0712ad46a0c87e896d8e42c159993f402dd740
Hamayun Afridi <hamayun.afridi99@gmail.com>
2018-04-24 04:29:39 -0700

    Resource Collection Created

---
+++ app/Http/Resources/Product/ProductCollection.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class ProductCollection extends Resource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+
+            'name' => $this->name,
+            'totalPrice' => abs((($this->price/100)*8) - ($this->price)),
+            'discount' => $this->discount,
+            'href' => [
+                    'link' => route('products.show', $this->id)
+            ]
+
+        ];
+    }
+}
